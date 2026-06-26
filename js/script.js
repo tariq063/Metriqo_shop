2f8e0cf4c201e8dcf2bb7ff5ec391dc667e41f46
Your GitHub Username <your-email@example.com>
2026-06-26 17:25:59 +0600

    update

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
 
    Leave ORDER_ENDPOINT = '' to run in demo mode (just shows a toast).
    ================================================================== */
-  const ORDER_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxyKacElqBnczOQpC0BT_okOPhLBlUXG4D3cYFGfaCxgQaO2IjLvYFq34a4pP3gMkH2EA/exec';            // <-- paste your URL here
+  const ORDER_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxwlGHShLhFyL11tQSajzJstKWmVHj_RKUelhjApYoZVO0a_EndFbn3H4Yq0pgy_Gk/exec';            // <-- paste your URL here
   const ENDPOINT_TYPE  = 'sheet';       // 'formspree' | 'sheet'
 
 (function () {
